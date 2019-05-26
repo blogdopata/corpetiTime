6eb4133d225dd9a26195f4308bda0a4c55fdddb9
unknown <vcaballero_c@hotmail.com>
2019-05-26 14:51:47 -0500

    fixeando  productos

--- FreeLTime/productos/js/controlsSlider.js
+++ FreeLTime/productos/js/controlsSlider.js
@@ -1,38 +1,72 @@
-
 // You can also use "$(window).load(function() {"
-$(function () {
+$(function() {
 
- 
-  // Slideshow 4
-  $("#slider4").responsiveSlides({
-    auto: false,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "callbacks",
-    before: function () {
-      $('.events').append("<li>before event fired.</li>");
-    },
-    after: function () {
-      $('.events').append("<li>after event fired.</li>");
-    }
-  });
+    /* Slider para Lapirceros   */
+    // Slideshow 4
+    $("#slider4").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function() {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function() {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
 
 
 
-   // Slideshow 4
-   $("#slider5").responsiveSlides({
-    auto: false,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "callbacks",
-    before: function () {
-      $('.events').append("<li>before event fired.</li>");
-    },
-    after: function () {
-      $('.events').append("<li>after event fired.</li>");
-    }
-  });
+    // Slideshow 5
+    $("#slider5").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function() {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function() {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+
+
+    /* Slider para llaveros  */
+    $("#slider6").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function() {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function() {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+
+
+    /* Slider Miscelaneas */
+    $("#slider7").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function() {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function() {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+
+
+
 
 });
